e5ded6c7e8e141e371025c9e577bb86ccd75b543
Atik Mahbub <amahbub10@github.com>
2021-02-10 16:53:26 +0600

    changed

--- src/Views/LandingPage/Home.js
+++ src/Views/LandingPage/Home.js
@@ -6,39 +6,32 @@ import { Link } from 'react-router-dom';
 function Home() {
     return (
         <>
-            <div className="row full-home">
-                <div className="landing-card-container">
-                        <div className="landing-chip"></div>
-                        <div className="landing-logo"> 
-                            <span></span> 
-                            <span></span>  
-                            <span></span>
-                        </div>
-                        <div className="landing-card-number">
-                            <span>7777 7777 7777 7777</span>
-                            <span className="landing-valid">Valid SAT 77/77</span>
-                        </div>
+            <div class="main">
+            <div class="d1"></div>
+            <div class="d2"></div>
+            <div class="d3"></div>
+            <div class="d4"></div>
+            <div className="cardNButton">
+                <div className="landing-card-container card-1">
+                    <div className="landing-chip"></div>
+                    <div className="landing-logo"> 
+                        <div>Your</div>
+                        <div>Logo</div>  
+                        <div>Here</div>
                     </div>
-                    <div className="landing-card-container card-1">
-                        <div className="landing-chip"></div>
-                        <div className="landing-logo"> 
-                            <div>Your</div>
-                        
-                            <div>Logo</div>  
-                            <div>Here</div>
-                        </div>
-                        <div className="landing-card-number">
-                            <span>7777 7777 7777 7777</span>
-                            <span className="landing-valid">Valid SAT 77/77</span>
-                        </div>
+                    <div className="landing-card-number">
+                        <span>7777 7777 7777 7777</span>
+                        <span className="landing-valid">Valid SAT 77/77</span>
                     </div>
-
-                    <div className="home-button">
+                </div>
+                <div className="home-button">
                             <Button component={ Link } to="/order" size="large" variant="outlined" color="default" >
                                 Customize Your Own Card
                             </Button>
-                    </div>
+                </div>
             </div>
+            
+        </div>
 
         </>
     )
